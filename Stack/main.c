f37c040075173add4fc42fd817942708d4ecee8b
Ahmed Hgazy <127811335+ahmedhgazy@users.noreply.github.com>
2024-11-08 07:32:22 +0200

    remove an Element from the stack with maintaining other element as it is

--- Stack/main.c
+++ Stack/main.c
@@ -4,6 +4,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+// remove an item from stack without changing the ordering of the stack 
+/*
+
+void removeElemnt(Stack *s, int item)
+{
+     type currentItem;
+    Stack temp;
+    createStack(&temp);
+
+
+    while (!isStackEmpty(*s)) {
+        currentItem = pop(s);
+        if (currentItem != item) {
+            push(currentItem, &temp);
+        }
+    }
+
+
+    while (!isStackEmpty(temp)) {
+        push(pop(&temp), s);
+    }
+
+}
+*/
 int main()
 {
     //! Tacking a line of text from the user and print it in a reverse order
@@ -54,5 +78,14 @@ int main()
     pop(&s);
     printf("\t\t The Top pointer of stack points to the top element and the value is: %d\t\t\n", peak(s));
 
+/*
+  removeElemnt(&s,3);
+    printf("from the method %d",s.top);
+
+    for(int i=0;i<MAX;i++){
+        printf(" \n item %d = %d\n",i+1,s.items[i]);
+    }
+*/
+    
     return 0;
 }
